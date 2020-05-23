d05e6fd0e8df6485da16d4b07e90c3d8068f483f
Bucky <buckyroberts@gmail.com>
2020-05-22 20:06:40 -0700

    Adding common validation

---
+++ thenewboston/transactions/validation.py
@@ -0,0 +1,12 @@
+def validate_transaction_exists(*, amount, error, recipient, txs):
+    """
+    Check for the existence of a Tx
+    """
+
+    tx = next((tx for tx in txs if tx.get('amount') == amount and tx.get('recipient') == recipient), None)
+    if not tx:
+        raise error({
+            'error_message': 'Tx not found',
+            'expected_amount': amount,
+            'expected_recipient': recipient
+        })
