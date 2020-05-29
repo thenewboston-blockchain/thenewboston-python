2569757ba09fbf87eb3d677c6edbf5741747f158
Bucky <buckyroberts@gmail.com>
2020-05-28 17:42:28 -0700

    Playground updates

--- playground/main.py
+++ playground/main.py
@@ -5,28 +5,33 @@ from thenewboston.verify_keys.verify_key import encode_verify_key, get_verify_ke
 from thenewboston.accounts.manage import create_account
 
 if __name__ == '__main__':
-    signing_key, account_number = create_account()
-    write_signing_key_file(signing_key, 'validator_signing_key_file')
-    # signing_key = read_signing_key_file('buckys_signing_key_file')
-    # account_number = get_verify_key(signing_key=signing_key)
+    # signing_key, account_number = create_account()
+    # write_signing_key_file(signing_key, 'validator_signing_key_file')
+
+    signing_key = read_signing_key_file('buckys_signing_key_file')
+    account_number = get_verify_key(signing_key=signing_key)
 
     encoded_account_number = encode_verify_key(verify_key=account_number)
     print(encoded_account_number)
 
-    # payments = [
-    #     {
-    #         'amount': 2,
-    #         'recipient': 'bank_001',
-    #     },
-    #     {
-    #         'amount': 2,
-    #         'recipient': 'validator_001',
-    #     }
-    # ]
-    # block = generate_block(
-    #     account_number=account_number,
-    #     balance_lock='484b3176c63d5f37d808404af1a12c4b9649cd6f6769f35bdf5a816133623fbc',
-    #     payments=payments,
-    #     signing_key=signing_key,
-    # )
-    # write_json('block.json', block)
+    payments = [
+        {
+            'amount': 2,
+            'recipient': 'bank_001',
+        },
+        {
+            'amount': 2,
+            'recipient': 'validator_001',
+        },
+        {
+            'amount': 90,
+            'recipient': 'min',
+        }
+    ]
+    block = generate_block(
+        account_number=account_number,
+        balance_lock='684b3176c63d5f37d808404af1a12c4b9649cd6f6769f35bdf5a816133623fbh',
+        payments=payments,
+        signing_key=signing_key,
+    )
+    write_json('block-4.json', block)
