c96685da8a14d73e7946abe6585aa9e799a6eff5
Bucky <buckyroberts@gmail.com>
2020-05-29 18:30:32 -0700

    Serializer update

--- thenewboston/serializers/confirmation_block_message.py
+++ thenewboston/serializers/confirmation_block_message.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 
+from thenewboston.constants.network import BLOCK_IDENTIFIER_LENGTH
 from thenewboston.serializers.account_balance import AccountBalanceSerializer
 from thenewboston.serializers.network_block import NetworkBlockSerializer
 
 
 class ConfirmationBlockMessageSerializer(serializers.Serializer):
     block = NetworkBlockSerializer()
+    block_identifier = serializers.CharField(max_length=BLOCK_IDENTIFIER_LENGTH)
     updated_balances = AccountBalanceSerializer(many=True)
 
     def create(self, validated_data):
