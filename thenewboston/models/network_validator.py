ed2b95b7aad74ad3488b65147169888944a415fd
Bucky <buckyroberts@gmail.com>
2020-05-30 10:54:43 -0700

    Adding new model

---
+++ thenewboston/models/network_validator.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from thenewboston.constants.network import HEAD_HASH_LENGTH
+from thenewboston.models.network_node import NetworkNode
+
+
+class NetworkValidator(NetworkNode):
+    head_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
+    root_account_file = models.URLField(max_length=1024)
+    root_account_file_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
+    seed_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
+
+    class Meta:
+        abstract = True
