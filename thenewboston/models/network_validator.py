094306a9882c68962ff1404523ab8e19c905697b
Bucky <buckyroberts@gmail.com>
2020-06-11 16:59:41 -0700

    Rename seed_hash to seed_block_hash

--- thenewboston/models/network_validator.py
+++ thenewboston/models/network_validator.py
@@ -8,7 +8,7 @@ class NetworkValidator(NetworkNode):
     head_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
     root_account_file = models.URLField(max_length=1024)
     root_account_file_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
-    seed_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
+    seed_block_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
 
     class Meta:
         abstract = True
