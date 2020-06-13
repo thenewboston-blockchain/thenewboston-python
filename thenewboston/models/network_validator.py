86017a9956ce125bf74623b31ca83f975e2c900f
Bucky <buckyroberts@gmail.com>
2020-06-13 07:20:21 -0700

    Remove head_hash from SQL

--- thenewboston/models/network_validator.py
+++ thenewboston/models/network_validator.py
@@ -5,7 +5,6 @@ from thenewboston.models.network_node import NetworkNode
 
 
 class NetworkValidator(NetworkNode):
-    head_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
     root_account_file = models.URLField(max_length=1024)
     root_account_file_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
     seed_block_hash = models.CharField(max_length=HEAD_HASH_LENGTH)
