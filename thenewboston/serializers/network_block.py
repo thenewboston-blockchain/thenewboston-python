4ec9a1585d65cccdaae82efc8ac1b75b0d6d7d21
Bucky <buckyroberts@gmail.com>
2020-06-13 09:57:38 -0700

    Update NetworkBlockSerializer

--- thenewboston/serializers/network_block.py
+++ thenewboston/serializers/network_block.py
@@ -1,13 +1,13 @@
 from rest_framework import serializers
 
 from thenewboston.constants.network import SIGNATURE_LENGTH, VERIFY_KEY_LENGTH
-from thenewboston.serializers.network_transaction import NetworkTransactionSerializer
+from thenewboston.serializers.message import MessageSerializer
 
 
 class NetworkBlockSerializer(serializers.Serializer):
     account_number = serializers.CharField(max_length=VERIFY_KEY_LENGTH)
+    message = MessageSerializer()
     signature = serializers.CharField(max_length=SIGNATURE_LENGTH)
-    txs = NetworkTransactionSerializer(many=True)
 
     def create(self, validated_data):
         pass
