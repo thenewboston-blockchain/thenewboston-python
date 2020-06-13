5ce38b9602c9c0192151ba5581ddb3c21dc01b54
Bucky <buckyroberts@gmail.com>
2020-06-13 10:20:07 -0700

    Update generate_balance_lock() for new block model

--- thenewboston/blocks/balance_lock.py
+++ thenewboston/blocks/balance_lock.py
@@ -3,9 +3,9 @@ from hashlib import sha3_256 as sha3
 from thenewboston.utils.tools import sort_and_encode
 
 
-def generate_balance_lock(*, tx):
+def generate_balance_lock(*, message):
     """
-    Generate a balance lock from a Tx
+    Generate a balance lock from a message
     """
 
-    return sha3(sort_and_encode(tx)).digest().hex()
+    return sha3(sort_and_encode(message)).digest().hex()
