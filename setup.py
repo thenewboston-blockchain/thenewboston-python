ba8a835d0258961071ec9e19296a312028ef8c46
Bucky <buckyroberts@gmail.com>
2020-07-18 15:21:55 -0700

    CORS package

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     install_requires=[
-        'Django==3.0.7',
+        'Django==3.0.8',
         'PTable==0.9.2',
         'PyNaCl==1.3.0',
+        'django-cors-headers==3.4.0',
         'djangorestframework==3.11.0',
         'pycodestyle==2.6.0',
         'requests~=2.23.0',
@@ -13,5 +14,5 @@ setup(
     packages=find_packages(
         exclude=['tests', 'tests.*']
     ),
-    version='0.0.4',
+    version='0.0.5',
 )
