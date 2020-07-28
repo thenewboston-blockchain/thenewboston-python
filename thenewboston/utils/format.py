fdf30043b79232848937629174f7ea441d7ad63d
Bucky Roberts <buckyroberts@gmail.com>
2020-07-28 12:13:08 -0700

    Remove dead code (#8)
    * remove unused code from utils
    
    * Cleaning
    
    Co-authored-by: grubox_rajat <rajat.sharma@grubox.in>

--- thenewboston/utils/format.py
+++ thenewboston/utils/format.py
@@ -5,14 +5,3 @@ def format_address(*, ip_address, port, protocol):
 
     port = f':{port}' if port else ''
     return f'{protocol}://{ip_address}{port}'
-
-
-def format_node_address(*, node):
-    """
-    Format address for node
-    """
-
-    ip_address = node.ip_address
-    port = f':{node.port}' if node.port else ''
-    protocol = node.protocol
-    return f'{protocol}://{ip_address}{port}'
