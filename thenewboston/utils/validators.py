4a7fb2825d003ed491664c2550f2618852484c1d
grubox_rajat <rajat.sharma@grubox.in>
2020-07-28 21:44:32 +0530

    remove unused code from utils

--- thenewboston/utils/validators.py
+++ thenewboston/utils/validators.py
@@ -25,15 +25,3 @@ def validate_is_real_number(value):
             _('Infinity is not allowed'),
             params={'value': value},
         )
-
-
-def validate_is_signature_length(value):
-    """
-    Validate value is length of signature
-    """
-
-    if len(value) != SIGNATURE_LENGTH:
-        raise ValidationError(
-            _(f'Length of {SIGNATURE_LENGTH} required'),
-            params={'value': value},
-        )
