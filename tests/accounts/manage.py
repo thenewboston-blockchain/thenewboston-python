9c4d2a02516be082503b102bc25fbc3aa722168a
Bucky <buckyroberts@gmail.com>
2020-08-27 15:56:07 -0700

    Tests

--- tests/accounts/manage.py
+++ tests/accounts/manage.py
@@ -5,5 +5,6 @@ from thenewboston.accounts.manage import create_account
 
 def test_create_account():
     signing_key, account_number = create_account()
+
     assert isinstance(signing_key, SigningKey)
     assert isinstance(account_number, VerifyKey)
