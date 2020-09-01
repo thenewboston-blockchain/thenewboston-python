3b8915f2cc409c03a0b907b4896d8805e10e8ddc
Bucky <buckyroberts@gmail.com>
2020-08-31 17:11:42 -0700

    Tx validation update

--- thenewboston/transactions/validation.py
+++ thenewboston/transactions/validation.py
@@ -3,7 +3,8 @@ def validate_transaction_exists(*, amount, error, recipient, txs):
     Check for the existence of a Tx
     """
 
-    tx = next((tx for tx in txs if tx.get('amount') == amount and tx.get('recipient') == recipient), None)
+    tx = next((tx for tx in txs if tx.get('amount') >= amount and tx.get('recipient') == recipient), None)
+
     if not tx:
         raise error({
             'error_message': 'Tx not found',
