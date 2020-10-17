7d540995ab48ec7b0a5ae5358f6142651f280ad1
Mikhail <5008719+fonar@users.noreply.github.com>
2020-10-17 21:29:30 +0300

    run tests in parallel #67 (#24)
    * run tests in parallel #67
    
    * Add package
    
    Co-authored-by: Bucky <buckyroberts@gmail.com>

---
+++ thenewboston/third_party/django_redis/pool.py
@@ -0,0 +1,10 @@
+from django_redis.pool import ConnectionFactory
+
+
+class FakeConnectionFactory(ConnectionFactory):
+    """
+    Used for faking Redis, as we don't want to implement fake Redis connection pooling.
+    """
+
+    def get_connection(self, params):
+        return self.redis_client_cls(**self.redis_client_cls_kwargs)
