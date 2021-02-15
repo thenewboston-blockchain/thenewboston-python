dbe65adc3756977c80d0be7363b1819f70426d14
DucPhamTV <phamcongduc1994@gmail.com>
2021-02-15 08:01:26 +0000

    Add "fee" to network_transaction factory

--- src/thenewboston/factories/network_transaction.py
+++ src/thenewboston/factories/network_transaction.py
@@ -1,13 +1,14 @@
-from factory import Faker
+from factory import Faker, Iterator
 from factory.django import DjangoModelFactory
 
-from ..constants.network import MAX_POINT_VALUE, MIN_POINT_VALUE, VERIFY_KEY_LENGTH
+from ..constants.network import ACCEPTED_FEE_LIST, MAX_POINT_VALUE, MIN_POINT_VALUE, VERIFY_KEY_LENGTH
 from ..models.network_transaction import NetworkTransaction
 
 
 class NetworkTransactionFactory(DjangoModelFactory):
     amount = Faker('pyint', max_value=MAX_POINT_VALUE, min_value=MIN_POINT_VALUE)
     recipient = Faker('text', max_nb_chars=VERIFY_KEY_LENGTH)
+    fee = Iterator(ACCEPTED_FEE_LIST + [''])
 
     class Meta:
         model = NetworkTransaction
