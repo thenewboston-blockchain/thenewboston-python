f4dc6b3cf8f4779539864ccf76644fe706a95d3f
achalpatel <achalpatel008@gmail.com>
2021-02-21 10:00:07 -0500

    Test added

--- tests/utils/format.py
+++
@@ -1,9 +0,0 @@
-from thenewboston.utils.format import format_address
-
-
-def test_format_address():
-    ip_address = '127.0.0.1'
-    port = '80'
-    protocol = 'http'
-    assert f'{protocol}://{ip_address}:{port}' == format_address(ip_address=ip_address, port=port, protocol=protocol)
-    assert f'{protocol}://{ip_address}' == format_address(ip_address=ip_address, port='', protocol=protocol)
